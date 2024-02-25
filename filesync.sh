11d9d9d8baaa17d70241a9d05f6e5397e580f750
Caleb Taber <calebtaber@gmail.com>
2024-02-25 14:24:13 -0500

    Fixed error where a file with a name containing a space would be considered two or more different files. Passes all tests

--- filesync.sh
+++ filesync.sh
@@ -30,7 +30,7 @@ if [[ -e "$blacklist" ]]; then
   cp -u "$remote_prefix/.sync_exclude" "$blacklist"
 fi
 # Clear trash directory if exists
-if [[ -e $trash ]]; then
+if [[ -e "$trash" ]]; then
     rm -r "$trash"
 fi
 mkdir -p "$trash/local"
@@ -39,20 +39,20 @@ mkdir -p "$trash/remote"
 
 # Check for verbose logging ($3)
 verbose_logging=0;
-if [[ $3 == "-v" ]]; then
+if [[ "$3" == "-v" ]]; then
   verbose_logging=1
 fi
 
 
 # Check that local directory exists
-if [[ ! -d $local_prefix ]]; then
+if [[ ! -d "$local_prefix" ]]; then
   echo "Error: local directory $local_prefix does not exist. Exiting"
   exit 1
 fi
 
 
 # Check that remote directory exists
-if [[ ! -d $remote_prefix ]]; then
+if [[ ! -d "$remote_prefix" ]]; then
   echo "Error: remote directory $remote_prefix does not exist. Exiting"
   exit 1
 fi
@@ -60,8 +60,8 @@ fi
 
 # $1 is the info to log
 log_extra_info () {
-  if [ $verbose_logging -eq 1 ]; then
-    echo $1
+  if [ "$verbose_logging" -eq "1" ]; then
+    echo "$1"
   fi
 }
 
@@ -71,24 +71,30 @@ log_extra_info () {
 trash_file () {
   local curr_dir=$(pwd)
   
-  if [[ $3 == "0" ]]; then
+  if [[ "$3" == "0" ]]; then
     mkdir -p "$trash/local/$1"
-    cd $local_prefix
+    cd "$local_prefix"
     mv "./$1/$2" "$trash/local/$1"
   elif [[ $3 == "1" ]]; then
     mkdir -p "$trash/remote/$1"
-    cd $remote_prefix
+    cd "$remote_prefix"
     mv "./$1/$2" "$trash/remote/$1"
   fi
   
-  cd $curr_dir
+  cd "$curr_dir"
 }
 
 # $1 is the relative path of the directory to sync
 recursive_directory_sync () {
-  mapfile -t filenames < <( find "$local_prefix/$1" "$remote_prefix/$1" -mindepth 1 -maxdepth 1 -printf "%f\n" | sort | uniq )
+  local filenames=()
   
-  for file_name in ${filenames[@]}; do
+  # Pipe errors from find to /dev/null because the local or remote path may not exist, which is not
+  # an error in this program
+  mapfile -t filenames < <( find "$local_prefix/$1" "$remote_prefix/$1" -mindepth 1 -maxdepth 1 -printf "%f\n" 2>/dev/null | sort | uniq )
+  
+  local num_files=${#filenames[@]}
+  for index in $(seq 0 $((num_files - 1))); do
+    local file_name=${filenames[index]}
     if [[ "$file_name" == ".last_sync" ]] || [[ "$file_name" == ".sync_trash" ]] || [[ "$file_name" == ".sync_exclude" ]]; then
       continue
     fi
@@ -101,22 +107,22 @@ recursive_directory_sync () {
     fi
     
     # If file exists locally and remotely
-    if [[ -e $local_path ]] && [[ -e $remote_path ]]; then
+    if [[ -e "$local_path" ]] && [[ -e "$remote_path" ]]; then
     
       # If both are regular files
-      if [[ -f $local_path ]] && [[ -f $remote_path ]]; then
-        if [[ $local_path -nt $last_sync ]] && [[ $local_path -nt $remote_path ]]; then
+      if [[ -f "$local_path" ]] && [[ -f "$remote_path" ]]; then
+        if [[ "$local_path" -nt "$last_sync" ]] && [[ "$local_path" -nt "$remote_path" ]]; then
           log_extra_info "Trash remote $1/$file_name and replace with local"
           trash_file "$1" "$file_name" 1
           cp $local_path "$remote_prefix/$1"
-        elif [[ $remote_path -nt $last_sync ]] && [[ $remote_path -nt $local_path ]]; then
+        elif [[ "$remote_path" -nt "$last_sync" ]] && [[ "$remote_path" -nt "$local_path" ]]; then
           log_extra_info "Trash local $1/$file_name and replace with remote"
           trash_file "$1" "$file_name" 0
           cp $remote_path "$local_prefix/$1"
         fi
       
       # If both are directories
-      elif [[ -d $local_path ]] && [[ -d $remote_path ]]; then
+      elif [[ -d "$local_path" ]] && [[ -d "$remote_path" ]]; then
         recursive_directory_sync "$1/$file_name"
       
       # If one is a file and one is a directory, or at least one is not a regular file or directory
@@ -125,14 +131,14 @@ recursive_directory_sync () {
       fi
     
     # If the file exists only locally
-    elif [[ ! -e $remote_path ]]; then
+    elif [[ ! -e "$remote_path" ]]; then
     
       # If modified after last sync, copy to remote
-      if [[ $local_path -nt $last_sync ]]; then
-        if [[ -f $local_path ]]; then
+      if [[ "$local_path" -nt "$last_sync" ]]; then
+        if [[ -f "$local_path" ]]; then
           log_extra_info "Copy file $1/$file_name to remote"
           cp $local_path "$remote_prefix/$1/"
-        elif [[ -d $local_path ]]; then
+        elif [[ -d "$local_path" ]]; then
           log_extra_info "Copy directory $1/$file_name to remote"
           mkdir $remote_path
           recursive_directory_sync "$1/$file_name"
@@ -150,11 +156,11 @@ recursive_directory_sync () {
     elif [[ ! -e $local_path ]]; then
     
       # If modified after last sync, copy to local
-      if [[ $remote_path -nt $last_sync ]]; then
-        if [[ -f $remote_path ]]; then
+      if [[ "$remote_path" -nt "$last_sync" ]]; then
+        if [[ -f "$remote_path" ]]; then
           log_extra_info "Copy file $1/$file_name to local"
           cp $remote_path "$local_prefix/$1/"
-        elif [[ -d $remote_path ]]; then
+        elif [[ -d "$remote_path" ]]; then
           log_extra_info "Copy directory $1/$file_name to local"
           mkdir $local_path
           recursive_directory_sync "$1/$file_name"
@@ -171,14 +177,14 @@ recursive_directory_sync () {
   done
 }
 
+
 # Start sync
 recursive_directory_sync ""
 
 # Flag to exit here when running tests
-if [[ $4 == "-t" ]]; then
+if [[ "$4" == "-t" ]]; then
   exit 0
 fi
-
 
 # Print out files trashed and remove permanently if user consents
 if [[ ! -z $(find "$trash" -mindepth 1 -print | grep -Eo "trash/(local|remote)/.*") ]]; then 
@@ -188,17 +194,17 @@ if [[ ! -z $(find "$trash" -mindepth 1 -print | grep -Eo "trash/(local|remote)/.
 
   echo ""
   read -p "Delete these permanently? [y/n] " delete
-  if [[ $delete == "y" ]]; then
+  if [[ "$delete" == "y" ]]; then
     rm -r "$trash"
   fi
 fi
 
 
 # Set new last_sync
-echo $(date +%s) > $local_prefix/.last_sync
+echo $(date +%s) > "$local_prefix/.last_sync"
 
 # Preserve timestamps with -p when copying last_sync to remote
-cp -p $local_prefix/.last_sync $remote_prefix/
+cp -p "$local_prefix/.last_sync" "$remote_prefix/"
 
 
 # Case 1: File exists locally and remotely
